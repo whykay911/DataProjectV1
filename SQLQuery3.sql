57e6cb63222593906144634d0c7825be73ac1852
Adepoju Oluyinka <whykay911@gmail.com>
2023-11-29 01:19:34 -0500

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[AddedVaccinatedPeople]
+  FROM [PersonalProject].[dbo].[PercentPopulationVacccinated]
